18be9b28ec4222bdab63842617407ae6180aa5bd
Aneesh <aneesh1701@gmail.com>
2024-01-19 18:45:54 +0530

    add --output flag

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ struct Args {
     /// Source File
     file: PathBuf,
 
-    #[arg(short, long, default_value_t = false)]
-    debug: bool,
+    #[arg(short, long)]
+    output: PathBuf,
 }
 
 fn main() {
@@ -25,32 +25,22 @@ fn main() {
             // Lexer
             let tokens = Token::lexer(&src);
 
-            if args.debug {
-                println!(
-                    "Lexed:\n{:?}\n",
-                    tokens.clone().collect::<Result<Vec<Token>, ()>>()
-                );
-            }
-
             let token_iter = tokens.spanned().map(|(tok, span)| match tok {
                 Ok(tok) => (tok, span.into()),
-                Err(_) => panic!("Lexer Error"),
+                Err(_) => panic!("lexer Error"),
             });
 
             let token_stream = Stream::from_iter(token_iter).spanned((src.len()..src.len()).into());
 
             // Parser
             match parser().parse(token_stream).into_result() {
-                Ok(ast) => {
-                    if args.debug {
-                        println!("Parsed:\n{:?}\n", ast);
-                        println!("Assembly:");
-                    }
-
+                Ok(ast) => fs::write(
+                    args.output,
                     ast.into_iter()
                         .map(|stmt| stmt.code_gen())
-                        .for_each(|s| println!("{s}"));
-                }
+                        .fold(String::new(), |s, x| s + &x),
+                )
+                .expect("failed to write to output file"),
                 Err(err) => {
                     err.into_iter().for_each(|err| println!("{:?}", err));
                 }
